ba86cf2cbc3ee0a2a70a5a6c8956294792a25805
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-05-08 00:21:00 -0700

    Update and rename Import.js to ImportScripts.js

--- ImportScripts.js
+++ ImportScripts.js
@@ -57,28 +57,3 @@
 
     // Send the request
     xhr.send();
-  </script>
-<div style="z-index: 99; opacity: 0.5; width: 100%; height: 10px; background-color: #333; position: fixed; top: 0; left: 0;"></div>
-<div style="z-index: 9; position: fixed; top: 8px; left: 0; width: 100%; height: 0; background-color: #444; overflow: hidden; transition: height 0.3s ease;">
-  <ul style="list-style: none; margin: 0; padding: 0;">
-<label for="iframe-url-input">Enter URL:</label>
-<input style="width: 220px;" type="text" id="iframe-url-input">
-<button onclick="updateIframe()">Update IFrame URL</button>
-  </ul>
-      <div style="width: 70px; height: auto; background-color: black; color: white; font-size: 20px; padding: 5px; opacity: 0.7;" id="fps-counter">FPS: Calculating...</div>
-</div>
-<iframe style="overflow: hidden; allowfullscreen frameborder: 0; position: absolute; z-index: 1; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 100%; height: 100%; margin: 0; padding: 0;" id="my-iframe" src="https://replit.com/@Ironbrodiet/Inconspicuous-Browser?embed=1&output=1#main.py" ></iframe>
-<script>
-  const menu = document.querySelector('div:nth-of-type(2)');
-  const menuToggle = document.querySelector('div:nth-of-type(1)');
-  let menuOpen = false;
-  
-  menuToggle.addEventListener('click', () => {
-    if (!menuOpen) {
-      menu.style.height = '50px';
-      menuOpen = true;
-    } else {
-      menu.style.height = '0';
-      menuOpen = false;
-    }
-  });
